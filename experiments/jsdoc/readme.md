3c7d1fa2f3992b616fae418e0eb09110c20d136e
Andrei <Andrei0686@mail.ru>
2025-04-21 09:51:07 +0500

    добавлен jsdoc

---
+++ experiments/jsdoc/readme.md
@@ -0,0 +1,26 @@
+
+
+
+запуск генерации документации
+`jsdoc .\iPortConnector.js -d .\docs\`
+
+если не сработает тогда через npx
+создать файл jsdoc.json в котором указать пути к файлам и выходной папке
+`
+{
+    "source": {
+        "include": [
+            "./iPortConnector.js"
+        ]
+    },
+    "opts": {
+        "destination": "./docs"
+    }
+}
+`
+и запустить:
+ `npx jsdoc -c .\jsdoc.json`
+
+ если возникает ошибка доступа при запуске (в windows), тогда сначало разрешить доступ так 
+
+ `Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass`
--- experiments/swagger-demo2/app.js
+++ experiments/swagger-demo2/app.js
@@ -86,39 +86,3 @@ app.listen(3000, () => {
   console.log('OpenAPI YAML: http://localhost:3000/openapi.yaml');
 });
 
-
-
-
-
-
-// GET /products // получает продукты
-// POST /products // создает продукт {name:"стул", description:"кухонный",...} возвращает  {idProduct:2 , name:"стул", description:"кухонный",...}
-// GET /products/{idProduct} // возвращает описание продукта
-// PATCH /products/{idProduct} // изменяет продукт {name:"стул", description:"обеденный"} возвращает  {idProduct:2 , name:"стул", description:"кухонный",...}
-
-// POST /users  // добавить пользователя {name:"Вася", phone:444444, address:"", region:"центральный" }
-// GET /users //выводит список кользователей
-// GET /users/{idUser} //выводит данные пользователя
-// PATCH /users/{idUser} // изменяет данные пользователя  {name:"Алеша Алексеевич" }  возвращает  данные пользователя  {name:"Алеша А", phone:444444, address:"", region:"центральный" }
-
-// POST /users/{idUser}/orders // { region:1} создается новый заказ или возвращается уже созданный {idUser:1, region:1, idOreder:45, products:[{idProduct:123, count:4},{idProduct:22, count:2,}]} 
-// POST /users/{idUser}/orders/{idOrder}   // добавление товара в заказ {idUser:1, region:1, idProduct:123, count:4,} возвращается весь заказ {idUser:1, region:1, idOreder:45, products:[{idProduct:123, count:4},{idProduct:22, count:2,}]} где idOreder номер открытого заказа
-// PATCH /users/{idUser}/orders/{idOrder}  // изменение количество товара в заказе {idUser:1, region:1, idProduct:123, count:7,} возвращается весь заказ {idUser:1, region:1, idOreder:45, products:[{idProduct:123, count:4,idProduct:22, count:2,}]} где idOreder номер открытого заказа
-// DELETE /users/{idUser}/orders/{idOrder}?idProduct={idProduct} // удаление товара из заказа. возвращается весь заказ {idUser:1, region:1, idOreder:45, products:[{idProduct:123, count:4,idProduct:22, count:2,}]} 
-// GET /users/{idUser}/orders/{idOrder} возвращается весь заказ {idUser:1, region:1, idOreder:45, products:[{idProduct:123, count:4,idProduct:22, count:2,}]} 
-// GET /users/{idUser}/orders?state="closed" возвращает закрытые заказы пользователя
-// GET /users/{idUser}/orsers?iduser={idUser}?
-
-
-// GET /prices?idProduct={idProduct} // получаем цену продукта и акцию со скидкой если она есть {idPrice:3, idProduct:4, idStorage:1, idPromotion: 4, price:332, sale:15 , count: 6} count количество для акции, sale скидка, price цена
-// GET /prices // получаем цены на все продукты [{idPrice:3, idProduct:4, idStorage:1, idPromotion: 4, price:332, sale:15 , count: 6},{idPrice:2, idProduct:4, idStorage:2, idPromotion: 4, price:332, sale:10 , count: 16}]
-
-
-
-// POST /storages/{idStorage}/reserves/{idProduct} // резервирование товара на складе передается { idOrder:3, count: 10 } return {idReserve:6, idProduct:5, idOrder:3, count: 3 } count - сколько удалось зарехервировать.
-// DELETE /storages/{idStorage}/reserves/{idProduct} // закрывает заказ удаляя из резерва товар { idOrder:3}
-// PATCH /storages/{idStorage}/reserves/{idProduct} // изменить количество резерва товара для заказа { idOrder:3, count: 20 } return {idReserve:6, idOrder:3, count: 18 } count - сколько удалось зарехервировать. idOrder -номер заказа пример для отмены резерва необходимо указать count=0
-// GET /storages // возвращает список складов [ {idStorage:1, name:"склад 1"}, {idStorage:2, name:"склад 2"}, {idStorage:3, name:"склад 3"}]
-// GET /storages/{idStorage}/reserves?idOrder=3 // возвращает все товары зарезервированные под заказ [ {idReserve:6, idProduct:5, idOrder:3, count: 3 }, {idReserve:7, idProduct:23, idOrder:3, count: 6 } ]
-
-
